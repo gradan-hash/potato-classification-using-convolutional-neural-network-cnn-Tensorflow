ed9f4fc9726bfcec8638cd291b030338ab75e028
gradan-hash <mutukunyaa@gmail.com>
2024-05-17 14:02:09 +0300

    cors

--- api/main.py
+++ api/main.py
@@ -8,11 +8,25 @@ from PIL import Image
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
-
+from fastapi.middleware.cors import CORSMiddleware
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",  # No trailing slash
+    "http://localhost:8000",  # If your FastAPI runs here too
+    "http://localhost",       # Covering general localhost
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # MODEL = tf.keras.models.load_model("C:/xampp/htdocs/potato classification/saved_model/1")
 
@@ -21,8 +35,8 @@ MODEL_PATH = "C:/xampp/htdocs/potato classification/saved_model/1"
 
 # Create a new Keras model with the TFSMLayer
 MODEL = keras.Sequential([
-    layers.InputLayer(input_shape=(256, 256, 3)),  # Adjust input_shape based on your model's requirements
-    layers.TFSMLayer(MODEL_PATH, call_endpoint='serving_default')  # You may need to change the `call_endpoint`
+    layers.InputLayer(shape=(256, 256, 3)),
+    layers.TFSMLayer(MODEL_PATH, call_endpoint='serving_default')
 ])
 
 CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
@@ -64,7 +78,6 @@ async def predict(file: UploadFile = File(...)):
     except Exception as e:
         logger.error(f"An error occurred: {str(e)}")
         return {"error": str(e)}
-
 
 
 if __name__ == "__main__":
