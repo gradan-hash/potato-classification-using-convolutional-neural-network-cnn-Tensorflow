b67b979551210763be82f5249ddc8aaf92301b72
gradan-hash <mutukunyaa@gmail.com>
2024-05-17 11:30:31 +0300

    model trainign and fast api endpoint

---
+++ api/main.py
@@ -0,0 +1,71 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.logger import logger
+import logging
+from uvicorn import run
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+app = FastAPI()
+
+# MODEL = tf.keras.models.load_model("C:/xampp/htdocs/potato classification/saved_model/1")
+
+# Define the path to the saved model
+MODEL_PATH = "C:/xampp/htdocs/potato classification/saved_model/1"
+
+# Create a new Keras model with the TFSMLayer
+MODEL = keras.Sequential([
+    layers.InputLayer(input_shape=(256, 256, 3)),  # Adjust input_shape based on your model's requirements
+    layers.TFSMLayer(MODEL_PATH, call_endpoint='serving_default')  # You may need to change the `call_endpoint`
+])
+
+CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
+
+
+@app.get("/ping")
+async def ping():
+    return "Hello, I am alive"
+
+
+def read_file_as_image(data) -> np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    return image
+
+
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    try:
+        image = read_file_as_image(await file.read())
+        img_batch = np.expand_dims(image, 0)
+
+        # Get predictions from the model
+        predictions = MODEL.predict(img_batch)
+        logger.info(f"Model output: {predictions}")
+
+        # Extract predictions using the correct key from the dictionary
+        predictions_array = predictions['dense_4']
+
+        # Determine the class with the highest probability
+        predicted_class_index = np.argmax(predictions_array)
+        predicted_class = CLASS_NAMES[predicted_class_index]
+        confidence = np.max(predictions_array)
+
+        logger.info(f"predicted_class: {predicted_class} confidence {confidence} ")
+        return {
+            'class': predicted_class,
+            'confidence': float(confidence)
+        }
+    except Exception as e:
+        logger.error(f"An error occurred: {str(e)}")
+        return {"error": str(e)}
+
+
+
+if __name__ == "__main__":
+    run(app, host='localhost', port=8000)
